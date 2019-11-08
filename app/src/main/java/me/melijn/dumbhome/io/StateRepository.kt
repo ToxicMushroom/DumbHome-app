ccb8190b64ac11d277ee730c5c5679268bfe28a7
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2019-11-08 18:43:31 +0100

    added cool await

--- app/src/main/java/me/melijn/dumbhome/io/StateRepository.kt
+++ app/src/main/java/me/melijn/dumbhome/io/StateRepository.kt
@@ -6,162 +6,145 @@ import kotlinx.coroutines.*
 import me.melijn.dumbhome.components.SwitchComponent
 import me.melijn.dumbhome.database.Database
 import me.melijn.dumbhome.ui.home.sub.DHItem
-
-import okhttp3.*
+import me.melijn.dumbhome.utils.await
+import okhttp3.MultipartBody
+import okhttp3.Request
 import org.json.JSONObject
-import java.io.IOException
 
 class StateRepository(private val preferenceMap: Map<String, *>, val context: Context) {
 
-    fun sendSwitchUpdate(switchItem: DHItem.SwitchItem) {
-        CoroutineScope(SupervisorJob() + Dispatchers.Main).launch {
-            withContext(Dispatchers.IO) {
-                val path1 = "switches/"
-                val path2 = "/state"
-                val networkType: String = if ((preferenceMap.getOrElse("remote_ip") {
-                        postError("remote ip not set")
+    fun sendSwitchUpdate(switchItem: DHItem.SwitchItem) =
+        CoroutineScope(SupervisorJob() + Dispatchers.IO).launch {
+            val path1 = "switches/"
+            val path2 = "/state"
+            val networkType: String = if ((preferenceMap.getOrElse("remote_ip") {
+                    postError("remote ip not set")
 
-                        return@withContext
-                    } as String) == IPCheckerRepository.publicIp) "local" else "remote"
+                    return@launch
+                } as String) == IPCheckerRepository.publicIp) "local" else "remote"
 
 
-                val protocol: String = if (preferenceMap.getOrElse(networkType + "_https") {
-                        postError("$networkType https not set")
+            val protocol: String = if (preferenceMap.getOrElse(networkType + "_https") {
+                    postError("$networkType https not set")
 
-                        return@withContext
-                    } as Boolean) "https" else "http"
+                    return@launch
+                } as Boolean) "https" else "http"
 
-                val port: Int = Integer.parseInt(preferenceMap.getOrElse(networkType + "_port") {
-                    postError("$networkType port not set")
+            val port: Int = Integer.parseInt(preferenceMap.getOrElse(networkType + "_port") {
+                postError("$networkType port not set")
 
-                    return@withContext
-                } as String)
+                return@launch
+            } as String)
 
-                val host: String = if (networkType == "local") {
-                    preferenceMap.getOrElse("local_ip") {
-                        postError("Local ip not set")
+            val host: String = if (networkType == "local") {
+                preferenceMap.getOrElse("local_ip") {
+                    postError("Local ip not set")
 
-                        return@withContext
-                    } as String
-                } else {
-                    preferenceMap.getOrElse("remote_hostname") {
-                        postError("Remote hostname not set")
-
-                        return@withContext
-                    } as String
-                }
-                val token: String = preferenceMap.getOrElse("token") {
-                    postError("Token not set")
-                    return@withContext
+                    return@launch
                 } as String
+            } else {
+                preferenceMap.getOrElse("remote_hostname") {
+                    postError("Remote hostname not set")
 
-                val requestBody = MultipartBody.Builder()
-                    .setType(MultipartBody.FORM)
-                    .addFormDataPart("state", switchItem.state.value.toString())
-                    .build()
+                    return@launch
+                } as String
+            }
+            val token: String = preferenceMap.getOrElse("token") {
+                postError("Token not set")
+                return@launch
+            } as String
 
-                val request = Request.Builder()
-                    .header("token", token)
-                    .url(
-                        "$protocol://$host:$port/$path1${switchItem.switchComponent.id}$path2"
-                    )
-                    .post(requestBody)
-                    .build()
+            val requestBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("state", switchItem.state.value.toString())
+                .build()
 
-                SyncDeviceRepository.client.newCall(request = request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                        postError("Request failed: ${e.message}")
-                    }
+            val request = Request.Builder()
+                .header("token", token)
+                .url(
+                    "$protocol://$host:$port/$path1${switchItem.switchComponent.id}$path2"
+                )
+                .post(requestBody)
+                .build()
 
-                    override fun onResponse(call: Call, response: Response) {
-                        println(response.body?.string())
-                    }
-                })
+            val response = SyncDeviceRepository.client.newCall(request = request).await { _, e ->
+                e.printStackTrace()
+                postError("Request failed: ${e.message}")
+            }
+            println(response.body?.string())
+        }
+
+    fun updateSwitchStates(switches: ArrayList<SwitchComponent>) =
+        CoroutineScope(SupervisorJob() + Dispatchers.IO).launch {
+
+            val path = "switches/states"
+            val networkType: String = if ((preferenceMap.getOrElse("remote_ip") {
+                    postError("remote ip not set")
+
+                    return@launch
+                } as String) == IPCheckerRepository.publicIp) "local" else "remote"
+
+
+            val protocol: String = if (preferenceMap.getOrElse(networkType + "_https") {
+                    postError("$networkType https not set")
+
+                    return@launch
+                } as Boolean) "https" else "http"
+
+            val port: Int = Integer.parseInt(preferenceMap.getOrElse(networkType + "_port") {
+                postError("$networkType port not set")
+
+                return@launch
+            } as String)
+
+            val host: String = if (networkType == "local") {
+                preferenceMap.getOrElse("local_ip") {
+                    postError("Local ip not set")
+
+                    return@launch
+                } as String
+            } else {
+                preferenceMap.getOrElse("remote_hostname") {
+                    postError("Remote hostname not set")
+
+                    return@launch
+                } as String
+            }
+            val token: String = preferenceMap.getOrElse("token") {
+                postError("Token not set")
+                return@launch
+            } as String
+            val request = Request.Builder()
+                .header("token", token)
+                .url(
+                    "$protocol://$host:$port/$path"
+                )
+                .build()
+
+            val response = SyncDeviceRepository.client.newCall(request = request).await { _, e ->
+                e.printStackTrace()
+                postError("Request failed: ${e.message}")
+            }
+
+            response.body?.string()?.let { it ->
+                val responseObj = JSONObject(it)
+                val switchArray = responseObj.getJSONArray("switches")
+                for (i in 0 until switchArray.length()) {
+                    val switch = JSONObject(switchArray.getString(i))
+
+                    val switchComponent =
+                        switches.find { match -> match.id == switch.getInt("id") }
+                    switches.removeAll { match -> match.id == switch.getInt("id") }
+                    switchComponent?.isOn = switch.getBoolean("state")
+                    switchComponent?.let { it1 -> switches.add(it1) }
+                }
+            }
+            CoroutineScope(SupervisorJob() + Dispatchers.Main).launch {
+                Database.switches.value = switches
             }
         }
-    }
 
-    fun updateSwitchStates(switches: ArrayList<SwitchComponent>) {
-
-        CoroutineScope(SupervisorJob() + Dispatchers.Main).launch {
-            withContext(Dispatchers.IO) {
-                val path = "switches/states"
-                val networkType: String = if ((preferenceMap.getOrElse("remote_ip") {
-                        postError("remote ip not set")
-
-                        return@withContext
-                    } as String) == IPCheckerRepository.publicIp) "local" else "remote"
-
-
-                val protocol: String = if (preferenceMap.getOrElse(networkType + "_https") {
-                        postError("$networkType https not set")
-
-                        return@withContext
-                    } as Boolean) "https" else "http"
-
-                val port: Int = Integer.parseInt(preferenceMap.getOrElse(networkType + "_port") {
-                    postError("$networkType port not set")
-
-                    return@withContext
-                } as String)
-
-                val host: String = if (networkType == "local") {
-                    preferenceMap.getOrElse("local_ip") {
-                        postError("Local ip not set")
-
-                        return@withContext
-                    } as String
-                } else {
-                    preferenceMap.getOrElse("remote_hostname") {
-                        postError("Remote hostname not set")
-
-                        return@withContext
-                    } as String
-                }
-                val token: String = preferenceMap.getOrElse("token") {
-                    postError("Token not set")
-                    return@withContext
-                } as String
-                val request = Request.Builder()
-                    .header("token", token)
-                    .url(
-                        "$protocol://$host:$port/$path"
-                    )
-                    .build()
-
-                SyncDeviceRepository.client.newCall(request = request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                        postError("Request failed: ${e.message}")
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        response.body?.string()?.let { it ->
-                            val responseObj = JSONObject(it)
-                            val switchArray = responseObj.getJSONArray("switches")
-                            for (i in 0 until switchArray.length()) {
-                                val switch = JSONObject(switchArray.getString(i))
-
-                                val switchComponent =
-                                    switches.find { match -> match.id == switch.getInt("id") }
-                                switches.removeAll { match -> match.id == switch.getInt("id") }
-                                switchComponent?.isOn = switch.getBoolean("state")
-                                switchComponent?.let { it1 -> switches.add(it1) }
-                            }
-                        }
-                        CoroutineScope(SupervisorJob() + Dispatchers.Main).launch {
-                            withContext(Dispatchers.Main) {
-                                Database.switches.value = switches
-                            }
-                        }
-                    }
-                })
-            }
-        }
-    }
-
-    fun postError(message: String) {
+    private fun postError(message: String) {
         CoroutineScope(SupervisorJob() + Dispatchers.Main).launch {
             withContext(Dispatchers.Main) {
                 Toast.makeText(context, message, Toast.LENGTH_LONG).show()
--- app/src/main/java/me/melijn/dumbhome/utils/ExtensionFunctions.kt
+++ app/src/main/java/me/melijn/dumbhome/utils/ExtensionFunctions.kt
@@ -1,6 +1,13 @@
 package me.melijn.dumbhome.utils
 
 import android.content.Context
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class ExtensionFunctions {
     fun init(context: Context) {
@@ -15,3 +22,39 @@ class ExtensionFunctions {
 fun Int.toPx(): Int = (this * ExtensionFunctions.displayDensity).toInt()
 
 fun Int.toDp(): Int = (this / ExtensionFunctions.displayDensity).toInt()
+
+
+suspend fun Call.await(failureHandler: ((call: Call, e: Exception) -> Unit)? = null): Response {
+    return suspendCancellableCoroutine { continuation ->
+        enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                continuation.resume(response)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                // Don't bother with resuming the continuation if it is already cancelled.
+                if (continuation.isCancelled) return
+                if (failureHandler != null) failureHandler.invoke(call, e)
+                else continuation.resumeWithException(e)
+            }
+        })
+
+        continuation.invokeOnCancellation {
+            try {
+                cancel()
+            } catch (ex: Throwable) {
+                //Ignore cancel exception
+            }
+        }
+    }
+}
+
+fun String.toUpperWordCase(): String {
+    var previous = ' '
+    var newString = ""
+    this.toCharArray().forEach { c: Char ->
+        newString += if (previous == ' ') c.toUpperCase() else c.toLowerCase()
+        previous = c
+    }
+    return newString
+}
